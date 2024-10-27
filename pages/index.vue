37e1d834f9bfd80fc17d84d5fd54fcaba510456e
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-10-27 18:16:42 +0600

    Landing page is created

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1 class="text-3xl text-orange-500 text-center">Welcome to my CRUD Project</h1>
+        <div class="flex justify-center mt-5">
+            <NuxtLink class="flex justify-center bg-orange-300 w-36 px-3 py-2 rounded-md" to="/tasks">Task page</NuxtLink>
+        </div> 
+        
+    </div>
+</template>
