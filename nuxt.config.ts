f16fc4263bf98ecd78a54b2679027963d89092bb
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-10-27 18:18:38 +0600

    Updated configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
-import {resolve} from "path"
+import {resolve} from "path";
 export default defineNuxtConfig({
   alias:{
     '@' : resolve(__dirname,"/"),
   },
-  css:["assets/main.scss"]
+  css: ['~/assets/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  
 })
